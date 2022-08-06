caba78043034e76f36fe74c37bfe3855c1279b14
Kyosuke <kyosuke@users.noreply.github.com>
2022-08-06 23:02:22 +0900

    Add `config.fish`

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set PNPM_HOME /Users/kyosuke/Library/pnpm
+  fish_add_path $PNPM_HOME
+  fish_add_path /Users/kyosuke/.npm_global/bin
+end
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 ln -sf ~/GitHub/kyosuke/dotfiles/.npmrc ~/.npmrc
+echo "✔️  ~/.npmrc"
+ln -sf ~/GitHub/kyosuke/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+echo "✔️  ~/.config/fish/config.fish"
