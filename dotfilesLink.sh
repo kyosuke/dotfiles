477884b5d9f0661c766a329a1dd6fe27fff744da
Kyosuke <kyosuke@users.noreply.github.com>
2022-08-15 12:29:10 +0900

    global gitignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,6 @@ ln -sf ~/GitHub/kyosuke/dotfiles/.npmrc ~/.npmrc
 echo "✔️  ~/.npmrc"
 ln -sf ~/GitHub/kyosuke/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 echo "✔️  ~/.config/fish/config.fish"
+mkdir -p ~/.config/git
+ln -sf ~/GitHub/kyosuke/dotfiles/.config/git/ignore ~/.config/git/ignore
+echo "✔️  ~/.config/git/ignore"
