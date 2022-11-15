704efaaf1366c28e28de3204de9e37b518ec25a2
Kyosuke <kyosuke@users.noreply.github.com>
2022-11-15 20:47:06 +0900

    use setenv & $HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
 if status is-interactive
   # npm global
-  fish_add_path /Users/kyosuke/.npm_global/bin
+  fish_add_path $HOME/.npm_global/bin
 
   # pnpm
-  set PNPM_HOME /Users/kyosuke/Library/pnpm
+  setenv PNPM_HOME $HOME/Library/pnpm
   fish_add_path $PNPM_HOME
 
   # deno
-  set DENO_INSTALL /Users/kyosuke/.deno
+  setenv DENO_INSTALL $HOME/.deno
   fish_add_path $DENO_INSTALL/bin
 end
