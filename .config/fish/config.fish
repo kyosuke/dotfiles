4117c7aa3bf31055a9f6006ac39e10c6ea6d1059
Kyosuke <kyosuke@users.noreply.github.com>
2022-11-15 20:31:09 +0900

    Add Deno's path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+  # npm global
+  fish_add_path /Users/kyosuke/.npm_global/bin
+
+  # pnpm
   set PNPM_HOME /Users/kyosuke/Library/pnpm
   fish_add_path $PNPM_HOME
-  fish_add_path /Users/kyosuke/.npm_global/bin
+
+  # deno
+  set DENO_INSTALL /Users/kyosuke/.deno
+  fish_add_path $DENO_INSTALL/bin
 end
