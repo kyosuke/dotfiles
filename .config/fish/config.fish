78d20138dc3d90ccc0111fb7b10eda7530b58f75
Kyosuke <kyosuke@users.noreply.github.com>
2023-08-16 22:56:34 +0900

    Add Bun

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,8 @@ if status is-interactive
   # deno
   setenv DENO_INSTALL $HOME/.deno
   fish_add_path $DENO_INSTALL/bin
+  
+  # Bun
+  setenv BUN_INSTALL $HOME/.bun
+  fish_add_path $BUN_INSTALL/bin
 end
