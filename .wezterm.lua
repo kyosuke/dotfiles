9d6a07269e84456a0480f4306ff04646ae3bf8c8
Kyosuke <kyosuke@users.noreply.github.com>
2025-02-11 23:35:35 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,12 @@
 local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-return {
-  default_prog = { '/usr/local/bin/fish', '-l' },
-  color_scheme = "Solarized Dark - Patched",
-  font = wezterm.font 'PlemolJP35 Console',
-  font_size = 14,
-}
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 50
+config.default_prog = { '/usr/local/bin/fish', '-l' }
+config.color_scheme = "Solarized Dark - Patched"
+config.font = wezterm.font 'PlemolJP35 Console'
+config.font_size = 14
+
+return config
+
