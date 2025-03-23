66a0e7c8e5f198537b21146dd84fd8aa86c765d5
Kyosuke Nakamura <kyosuke@users.noreply.github.com>
2025-03-23 13:55:46 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ if status is-interactive
   # deno
   setenv DENO_INSTALL $HOME/.deno
   fish_add_path $DENO_INSTALL/bin
-  
+
   # Bun
   setenv BUN_INSTALL $HOME/.bun
   fish_add_path $BUN_INSTALL/bin
